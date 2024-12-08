9169fa08f33d8ba9b42f1ba8956a579c2a9cc350
Sathvik Bhagavan <sathvikbhagavan@gmail.com>
2024-12-08 19:07:19 +0100

    Running job with run name: colorful-dawn-4, url: https://wandb.ai/sathvikbhagavan-epfl/Spline-PINNs_without_heat_dp1/runs/lj3lo74x

--- src/inference.py
+++ src/inference.py
@@ -12,17 +12,17 @@ def plot_aginast_data(folder_path, vx_pred, vy_pred, vz_pred, p_pred):
     vz_inlet = np.load(os.path.join(folder_path, "vel_z_inlet.npy"))
     p = np.load(os.path.join(folder_path, "press.npy"))
     all_points = np.concatenate((vx_inlet[:, :3], vx[:, :3]))
-    fields = [
+    _fields = [
         [vx_inlet, vx, vx_pred, "vx"],
         [vy_inlet, vy, vy_pred, "vy"],
         [vz_inlet, vz, vz_pred, "vz"],
     ]
-    for field in fields:
-        scalar_field = np.concatenate((field[0][:, 3], field[1][:, 3]))
+    for _field in _fields:
+        scalar_field = np.concatenate((_field[0][:, 3], _field[1][:, 3]))
         index = np.random.choice(all_points.shape[0], 100000, replace=False)
         selected_points = all_points[index]
         selected_scalar_field = scalar_field[index]
-        selected_scalar_pred = field[2][index]
+        selected_scalar_pred = _field[2][index]
         # Create a 3D scatter plot using Plotly
         fig = go.Figure(
             data=[
@@ -36,7 +36,7 @@ def plot_aginast_data(folder_path, vx_pred, vy_pred, vz_pred, p_pred):
                         color=selected_scalar_field
                         - selected_scalar_pred,  # Color by scalar field
                         colorscale="Viridis",  # Color map
-                        colorbar=dict(title=f"{field[3]}"),  # Colorbar with title
+                        colorbar=dict(title=f"{_field[3]}"),  # Colorbar with title
                     ),
                 )
             ]
@@ -49,11 +49,38 @@ def plot_aginast_data(folder_path, vx_pred, vy_pred, vz_pred, p_pred):
                 zaxis_title="Z",
                 aspectmode="data",  # Ensures aspect ratio is based on data's extents
             ),
-            title=f"Diff in true and pred for {field[3]}",
+            title=f"Diff in true and pred for {_field[3]}",
         )
-        fig.write_html(f"../run/{field[3]}_diff.html")
+        fig.write_html(f"../run/{_field[3]}_diff.html")
+        fig = go.Figure(
+            data=[
+                go.Scatter3d(
+                    x=selected_points[:, 0],
+                    y=selected_points[:, 1],
+                    z=selected_points[:, 2],
+                    mode="markers",
+                    marker=dict(
+                        size=5,
+                        color=selected_scalar_field,
+                        colorscale="Viridis",  # Color map
+                        colorbar=dict(title=f"{_field[3]}"),  # Colorbar with title
+                    ),
+                )
+            ]
+        )
+        # Update layout with aspectmode='data' to preserve aspect ratio
+        fig.update_layout(
+            scene=dict(
+                xaxis_title="X",
+                yaxis_title="Y",
+                zaxis_title="Z",
+                aspectmode="data",  # Ensures aspect ratio is based on data's extents
+            ),
+            title=f"Data for {_field[3]}",
+        )
+        fig.write_html(f"../run/{_field[3]}_data.html")
         print(
-            f"The mean of {field[3]} differences is: {np.mean(scalar_field - field[2])}"
+            f"The mean of {_field[3]} differences is: {np.mean(scalar_field - _field[2])}"
         )
 
     scalar_field = p[:, 3]
